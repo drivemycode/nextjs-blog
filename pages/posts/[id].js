3c6c4be4ecbe028830d8646c99f31a224957fa26
drivemycode <107122532+drivemycode@users.noreply.github.com>
2022-08-09 17:54:23 +0800

    Added, implemented markdown package

---
+++ pages/posts/[id].js
@@ -0,0 +1,33 @@
+import { getAllPostsIds, getPostData } from "../../lib/posts";
+import Layout from '../../components/layout'
+
+export async function getStaticProps({params}) {
+    const postData = await getPostData(params.id)
+    return {
+        props: {
+            postData,
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostsIds();
+    return {
+        paths,
+        fallback: false,
+    }
+}
+
+export default function Post({ postData }) {
+    return (
+      <Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+      </Layout>
+    );
+  }
